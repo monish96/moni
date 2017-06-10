a788ef34dd8482a5f0cc73ff76ea0642aafe58f2
Monish N <knockme96@gmail.com>
2017-06-10 13:10:32 +0530

    Create aaer.c

---
+++ aaer.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int c(const void *d,const void *n)
+{
+return (*(char*)n)-(*(char*)d);
+}
+int main()
+{
+int n,i;
+scanf("%d",&n);
+char str[2*n];
+int j=n;
+while(i<n)
+{
+i=0;
+str[i]='B';
+str[j++]='W';
+i++;
+}
+str[n*2]='\0';
+qsort(str,2*nsizeof(char),c);
+printf("%s",str);
+return 0;
+}
